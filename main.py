bc9ba1875d9fdf2c8bed437170fb71e8d9020474
kimeong <109575843+kimeong@users.noreply.github.com>
2022-08-04 16:37:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+view = [300,200,100,500]
+show_raw = st.checkbox('show raw data')
+st.write('raw data')
+view
+st.write('# table')
+st.table(view)
+st.write('# bar graph')
+st.bar_chart(view)
